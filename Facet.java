22f00c327f73e2bc480f8172163499a0019ca8f9
broditsalera <56994255+broditsalera@users.noreply.github.com>
2019-12-09 11:05:06 +0300

    Add files via upload

---
+++ Facet.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+public class Facet {
+    private Vector[] vertex;
+    //Color color;
+    public Facet(Vector v1, Vector v2, Vector v3, Vector v4){
+        vertex[0] = v1;
+        vertex[1] = v2;
+        vertex[2] = v3;
+        vertex[3] = v4;
+    }
+    public void print(){
+        //закручиваем обход граней по часовой
+
+    }
+}
